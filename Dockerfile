225c20af47be6f336cef39b0b5b386c5167e5b26
Paul Xue <paul.xue@pressly.com>
2016-06-15 20:38:56 -0400

    fuck docker

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 FROM golang:1.6.2
 
-RUN apt-get update && apt-get install --no-install-recommends -y ca-certificates xmlsec1
-
 # API
 ADD . /go/src/bitbucket.org/moodie-app/moodie-api
 WORKDIR /go/src/bitbucket.org/moodie-app/moodie-api
-
-RUN echo $PWD
-RUN ls -al
-
 RUN make build
-COPY bin/api /bin/api
+RUN mv ./bin/api /bin/api
 
 EXPOSE 5331
 
--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"os"
+	"syscall"
+	"time"
+
+	"bitbucket.org/moodie-app/moodie-api/config"
+	"bitbucket.org/moodie-app/moodie-api/data"
+	"bitbucket.org/moodie-app/moodie-api/lib/connect"
+	"github.com/goware/lg"
+	"github.com/zenazn/goji/graceful"
+	"github.com/zenazn/goji/web"
 )
 
 var (
@@ -11,43 +20,40 @@ var (
 )
 
 func main() {
-	//conf, err := config.NewFromFile(*confFile, os.Getenv("CONFIG"))
-	//if err != nil {
-	//lg.Fatal(err)
-	//}
+	conf, err := config.NewFromFile(*confFile, os.Getenv("CONFIG"))
+	if err != nil {
+		lg.Fatal(err)
+	}
 
-	// SETUP
-	// [db]
-	//if err := data.NewDBSession(conf.DB); err != nil {
-	//lg.Fatal(err)
-	//}
+	//[db]
+	if err := data.NewDBSession(conf.DB); err != nil {
+		lg.Fatal(err)
+	}
 
-	// [connect]
-	//connect.Configure(conf.Connect)
+	//[connect]
+	connect.Configure(conf.Connect)
 
-	// [jwt]
-	//data.SetupJWTAuth(conf.Jwt.Secret)
+	//[jwt]
+	data.SetupJWTAuth(conf.Jwt.Secret)
 
-	//graceful.AddSignal(syscall.SIGINT, syscall.SIGTERM)
-	//graceful.Timeout(10 * time.Second) // Wait timeout for handlers to finish.
-	//graceful.PreHook(func() {
-	//lg.Info("waiting for requests to finish..")
-	//})
-	//graceful.PostHook(func() {
-	//lg.Info("finishing up...")
-	//if err := data.DB.Close(); err != nil {
-	//lg.Alert(err)
-	//}
-	//})
+	graceful.AddSignal(syscall.SIGINT, syscall.SIGTERM)
+	graceful.Timeout(10 * time.Second) // Wait timeout for handlers to finish.
+	graceful.PreHook(func() {
+		lg.Info("waiting for requests to finish..")
+	})
+	graceful.PostHook(func() {
+		lg.Info("finishing up...")
+		if err := data.DB.Close(); err != nil {
+			lg.Alert(err)
+		}
+	})
 
-	//lg.Infof("API starting on %v", conf.Bind)
+	lg.Infof("API starting on %v", conf.Bind)
 
-	//router := web.New()
-	//if err := graceful.ListenAndServe(conf.Bind, router); err != nil {
-	//lg.Fatal(err)
-	//}
+	router := web.New()
+	if err := graceful.ListenAndServe(conf.Bind, router); err != nil {
+		lg.Fatal(err)
+	}
 
-	//graceful.Wait()
-
-	fmt.Println("what.")
+	graceful.Wait()
 }
