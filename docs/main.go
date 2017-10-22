9980450a9dfdcf21dd8000953bb7caa9db908a45
Paul Xue <xuepaul.xp@gmail.com>
2017-10-22 19:07:17 -0400

    skip doc-gen and use lg

--- docs/main.go
+++ docs/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 
 	"bitbucket.org/moodie-app/moodie-api/web"
 	"github.com/go-chi/docgen"
-	"github.com/pressly/lg"
 )
 
 func main() {
@@ -15,11 +15,11 @@ func main() {
 	// mkdown output
 	mdDocFile, err := os.Create("./docs/routes.md")
 	if err != nil {
-		lg.Fatal(err)
+		log.Fatal(err)
 	}
 	mdDoc := &docgen.MarkdownDoc{Router: routes}
 	if err := mdDoc.Generate(); err != nil {
-		lg.Fatal(err)
+		log.Fatal(err)
 	}
 	mdDocFile.WriteString(mdDoc.String())
 	mdDocFile.Close()
@@ -27,7 +27,7 @@ func main() {
 	// json output
 	jsDocFile, err := os.Create("./docs/routes.json")
 	if err != nil {
-		lg.Fatal(err)
+		log.Fatal(err)
 	}
 	encoder := json.NewEncoder(jsDocFile)
 	encoder.SetIndent("", "  ")
