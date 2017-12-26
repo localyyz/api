0c7727bf3f5c4390bdd01f6970ca718c7b2d1b41
Paul Xue <xuepaul.xp@gmail.com>
2017-12-26 17:23:48 -0500

    cache active place in webhook context, dockerignore

--- web/shopify/shopify.go
+++ web/shopify/shopify.go
@@ -8,10 +8,13 @@ import (
 	"bitbucket.org/moodie-app/moodie-api/data"
 	"bitbucket.org/moodie-app/moodie-api/lib/shopify"
 	"bitbucket.org/moodie-app/moodie-api/web/api"
+	db "upper.io/db.v3"
 
 	"github.com/go-chi/render"
 	"github.com/pressly/lg"
 )
+
+var storeCache map[string]*data.Place
 
 func ShopifyStoreWhCtx(next http.Handler) http.Handler {
 	handler := func(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +30,16 @@ func ShopifyStoreWhCtx(next http.Handler) http.Handler {
 		shopID := parts[0]
 		ctx := r.Context()
 
-		place, err := data.DB.Place.FindByShopifyID(shopID)
-		if err != nil {
-			lg.Alertf("webhooks: place(%s) errored with: %+v", shopID, err)
-			render.Respond(w, r, err)
-			return
+		place, ok := storeCache[shopID]
+		if !ok {
+			var err error
+			place, err = data.DB.Place.FindByShopifyID(shopID)
+			if err != nil {
+				lg.Alertf("webhooks: place(%s) errored with: %+v", shopID, err)
+				render.Respond(w, r, err)
+				return
+			}
+			storeCache[place.ShopifyID] = place
 		}
 		// log the place context
 		lg.SetEntryField(ctx, "place_id", place.ID)
@@ -54,4 +62,17 @@ func ShopifyStoreWhCtx(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 	return http.HandlerFunc(handler)
+}
+
+func init() {
+	places, err := data.DB.Place.FindAll(db.Cond{"status": data.PlaceStatusActive})
+	if err != nil {
+		lg.Alert("failed to cache place id at init")
+		return
+	}
+
+	storeCache = make(map[string]*data.Place)
+	for _, p := range places {
+		storeCache[p.ShopifyID] = p
+	}
 }
