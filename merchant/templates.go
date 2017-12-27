40ebed25d7fb30978f2e19523ff0bfa53ab30187
Paul Xue <xuepaul.xp@gmail.com>
2017-12-26 23:32:50 -0500

    add merchant index.html to docker, fix location

--- merchant/templates.go
+++ merchant/templates.go
@@ -3,5 +3,5 @@ package merchant
 import "github.com/flosch/pongo2"
 
 var (
-	indexTmpl = pongo2.Must(pongo2.FromFile("merchant/index.html"))
+	indexTmpl = pongo2.Must(pongo2.FromFile("/merchant/index.html"))
 )
