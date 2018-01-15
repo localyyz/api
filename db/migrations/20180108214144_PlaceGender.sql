ccae5c79e3361ffdf2c611d4c7457f781517b244
Paul Xue <xuepaul.xp@gmail.com>
2018-01-15 10:38:13 -0500

    update old migrations

--- db/migrations/20180108214144_PlaceGender.sql
+++ db/migrations/20180108214144_PlaceGender.sql
@@ -1,7 +1,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE places ADD COLUMN gender smallint DEFAULT 0 NOT NULL;
+ALTER TABLE places ADD COLUMN gender smallint DEFAULT 3 NOT NULL;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
--- db/migrations/20180108224717_ProductGender.sql
+++ db/migrations/20180108224717_ProductGender.sql
@@ -2,6 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE products ADD COLUMN gender smallint DEFAULT 0 NOT NULL;
+CREATE INDEX product_gender_idx ON products (gender);
 
 
 -- +goose Down
