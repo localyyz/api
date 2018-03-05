8a6cbd2a97a2e8aebc97b99c4e04bd42516430d2
Paul Xue <xuepaul.xp@gmail.com>
2018-03-05 13:37:05 -0500

    feature: product size sorter

---
+++ lib/apparelsorter/sizes.go
@@ -0,0 +1,110 @@
+package apparelsorter
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+const shoesizes = `([0-9]+)(\.([0-9]+))?`
+
+var apparelsizes = []string{
+	"^osfa.*$",
+	"^one .*$",
+	"^one$",
+	"^xxs",
+	"^xs .*$",
+	"^x sm.*$",
+	"^xs.*$",
+	"^.* xs$",
+	"^xs",
+	"^extra small$",
+	"^sm.*$",
+	"^.* small",
+	"^ss",
+	"^short sleeve",
+	"^ls",
+	"^long sleeve",
+	"^s$",
+	"^small.*$",
+	`^s\/.*$`,
+	`^s \/.*$`,
+	"^s .*$",
+	"^m$",
+	"^medium.*$",
+	"^.*med.*$",
+	"^m .*$",
+	"^m[A-Za-z]*",
+	`^M\/.*$`,
+	"^l$",
+	"^.*lg.*$",
+	"^large.*$",
+	"^l .*$",
+	`^l\/.*$`,
+	"^lt$",
+	"^xl.*$",
+	"^extra large$",
+	"^x large.*$",
+	"^.* XL$",
+	"^x-l.*$",
+	"^l[A-Za-z]*$",
+	"^petite l.*$",
+	"^1x.*$",
+	"^.* 1x$",
+	"^2x.*$",
+	"^.* 2X$",
+	"^.*XXL.*$",
+	"^3x.*$",
+	"^4x.*$",
+	"^5x.*$",
+	"^6x.*$",
+	"^7x.*$",
+	"^8x.*$",
+	"^9x.*$",
+	"^10x.*$",
+	"^11x.*$",
+	"^12x.*$",
+	"^13x.*$",
+	"^14x.*$",
+	"^15x.*$",
+	"^16x.*$",
+	"^17x.*$",
+	"^18x.*$",
+}
+
+var (
+	apparelRegexes []*regexp.Regexp
+	shoeRegex      *regexp.Regexp
+)
+
+func matchSize(s string) *Size {
+	for i, re := range apparelRegexes {
+		if re.MatchString(s) {
+			return &Size{Size: s, Order: i}
+		}
+	}
+	if part := shoeRegex.FindString(s); len(part) > 0 {
+		// check if integer or float
+		s := &Size{Size: s}
+		if o, err := strconv.Atoi(part); err == nil {
+			// Store as normalized integer
+			s.Order = o * 1000
+		} else {
+			// Store as normalize float converted to integer
+			if o, err := strconv.ParseFloat(part, 64); err == nil {
+				s.Order = int(o * 1000.0)
+			}
+		}
+		return s
+	}
+	return nil
+}
+
+func init() {
+	for _, rx := range apparelsizes {
+		ri := fmt.Sprintf("(?i)%s", rx)
+		re := regexp.MustCompile(ri)
+		apparelRegexes = append(apparelRegexes, re)
+	}
+	shoeRegex = regexp.MustCompile(shoesizes)
+}
---
+++ lib/apparelsorter/sort_test.go
@@ -0,0 +1,54 @@
+package apparelsorter
+
+import (
+	"testing"
+)
+
+type sizeTest struct {
+	name     string
+	inputs   []string
+	expected []*Size
+}
+
+func TestSizeSorter(t *testing.T) {
+	t.Parallel()
+
+	tests := []sizeTest{
+		{"l m s", []string{"l", "s", "m"}, []*Size{{Size: "s"}, {Size: "m"}, {Size: "l"}}},
+		{"l m xs", []string{"l", "xs", "m"}, []*Size{{Size: "xs"}, {Size: "m"}, {Size: "l"}}},
+		{"large medium small", []string{"large", "small", "medium"}, []*Size{{Size: "small"}, {Size: "medium"}, {Size: "large"}}},
+		{"extra small", []string{"large", "small", "extra small"}, []*Size{{Size: "extra small"}, {Size: "small"}, {Size: "large"}}},
+		{"extra large", []string{"large", "small", "extra large"}, []*Size{{Size: "small"}, {Size: "large"}, {Size: "extra large"}}},
+		{"xxlarge", []string{"medium", "small", "xxlarge"}, []*Size{{Size: "small"}, {Size: "medium"}, {Size: "xxlarge"}}},
+		{"xxl", []string{"m", "s", "xxl"}, []*Size{{Size: "s"}, {Size: "m"}, {Size: "xxl"}}},
+		{"2xl", []string{"2xl", "l", "xl"}, []*Size{{Size: "l"}, {Size: "xl"}, {Size: "2xl"}}},
+		{"5 8 10", []string{"5", "10", "8"}, []*Size{{Size: "5"}, {Size: "8"}, {Size: "10"}}},
+		{"5 8 10.0", []string{"5", "10.0", "8"}, []*Size{{Size: "5"}, {Size: "8"}, {Size: "10.0"}}},
+		{"5.5 6 6.5", []string{"6.5", "5.5", "6", "7", "5"}, []*Size{{Size: "5"}, {Size: "5.5"}, {Size: "6"}, {Size: "6.5"}, {Size: "7"}}},
+		{"shoe size with string", []string{"5-men", "10-men", "8-men"}, []*Size{{Size: "5-men"}, {Size: "8-men"}, {Size: "10-men"}}},
+		{"shoe size with string 2", []string{"women-5", "women-10", "women-8"}, []*Size{{Size: "women-5"}, {Size: "women-8"}, {Size: "women-10"}}},
+		{"shoe size with string mixed float", []string{"women-5.5", "women-5", "women-8"}, []*Size{{Size: "women-5"}, {Size: "women-5.5"}, {Size: "women-8"}}},
+		{"mixed eu and us", []string{"40 eur - 7 us", "39 eur - 6 us", "41 eur - 8 us"}, []*Size{{Size: "39 eur - 6 us"}, {Size: "40 eur - 7 us"}, {Size: "41 eur - 8 us"}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := New(tt.inputs...)
+			Sort(actual)
+
+			tt.compare(t, actual)
+		})
+	}
+}
+
+func (tt sizeTest) compare(t *testing.T, actual []*Size) {
+	if len(tt.expected) != len(actual) {
+		t.Fatalf("test '%s': expected list length '%d', got '%d'", tt.name, len(tt.expected), len(actual))
+	}
+
+	for i, e := range tt.expected {
+		if e.Size != actual[i].Size {
+			t.Errorf("test '%s': expected size '%s' at %d, got '%s'", tt.name, e.Size, i, actual[i].Size)
+		}
+	}
+}
