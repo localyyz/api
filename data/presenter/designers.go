299c08f838f2bacdb69758b32884f4d1e71b026d
Paul Xue <xuepaul.xp@gmail.com>
2018-05-08 16:23:51 -0400

    hotfix: brand speed optimzation

--- data/presenter/designers.go
+++ data/presenter/designers.go
@@ -3,9 +3,12 @@ package presenter
 import (
 	"context"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"bitbucket.org/moodie-app/moodie-api/data"
 	"github.com/go-chi/render"
+	"github.com/pressly/lg"
 	db "upper.io/db.v3"
 )
 
@@ -51,41 +54,50 @@ func NewDesigner(ctx context.Context, product *data.Product) *Designer {
 	}
 
 	{ // product count
-		cond := db.Cond{"deleted_at": nil}
-		if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
-			cond["gender"] = gender
-		}
-		count, _ := data.DB.Product.Find(
-			db.And(
-				db.Raw(`tsv @@ plainto_tsquery(?)`, d.ID),
-				cond,
-			),
-		).Count()
-		d.ProductCount = int(count)
-	}
-
-	{ // product preview
-		cond := db.Cond{"p.deleted_at": nil}
-		if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
-			cond["p.gender"] = gender
-		}
-
-		var products []*data.Product
-		data.DB.Select("p.*").
-			From("products p").
-			Where(
+		if cache, ok := ctx.Value("count.cache").(map[string]int); ok {
+			d.ProductCount = cache[d.ID]
+		} else {
+			cond := db.Cond{"deleted_at": nil}
+			if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
+				cond["gender"] = gender
+			}
+			count, _ := data.DB.Product.Find(
 				db.And(
 					db.Raw(`tsv @@ plainto_tsquery(?)`, d.ID),
 					cond,
 				),
-			).
-			Limit(10).
-			All(&products)
+			).Count()
+			d.ProductCount = int(count)
+		}
+	}
 
-		if pp := NewProductList(ctx, products); len(pp) > 4 {
-			d.Products = pp[:4]
+	{
+		if productCache, ok := ctx.Value("product.cache").(map[string][]render.Renderer); ok {
+			d.Products = productCache[d.ID]
 		} else {
-			d.Products = pp
+			// product preview
+			cond := db.Cond{"p.deleted_at": nil}
+			if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
+				cond["p.gender"] = gender
+			}
+
+			var products []*data.Product
+			data.DB.Select("p.*").
+				From("products p").
+				Where(
+					db.And(
+						db.Raw(`tsv @@ plainto_tsquery(?)`, d.ID),
+						cond,
+					),
+				).
+				Limit(10).
+				All(&products)
+
+			if pp := NewProductList(ctx, products); len(pp) > 4 {
+				d.Products = pp[:4]
+			} else {
+				d.Products = pp
+			}
 		}
 	}
 
@@ -101,8 +113,108 @@ func (d *Designer) Render(w http.ResponseWriter, r *http.Request) error {
 
 type DesignerList []*Designer
 
+var splitRegex = regexp.MustCompile("[^a-zA-Z0-9-]+")
+
+func fetchDesignerProducts(ctx context.Context, query []string) []render.Renderer {
+	genderCond := []data.ProductGender{
+		data.ProductGenderUnisex,
+		data.ProductGenderMale,
+		data.ProductGenderFemale,
+	}
+	if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
+		genderCond = []data.ProductGender{data.ProductGender(gender)}
+	}
+
+	// bulk fetch the preview products
+	var p []*data.Product
+	err := data.DB.Select("*").
+		From(db.Raw(`(
+				SELECT *, row_number() over (partition by lower(brand)) as n
+				FROM products
+				WHERE tsv @@ to_tsquery(?)
+				AND deleted_at IS NULL
+				AND gender IN ?
+				) x`,
+			strings.Join(query, "|"),
+			genderCond,
+		)).
+		Where("n < 4").
+		All(&p)
+
+	if err != nil {
+		return nil
+	}
+	return NewProductList(ctx, p)
+}
+
+func fetchDesignerCount(ctx context.Context, parsed []string) map[string]int {
+	// bulk fetch product counts
+	cond := db.Cond{"deleted_at": nil}
+	if gender, ok := ctx.Value("session.gender").(data.UserGender); ok {
+		cond["gender"] = gender
+	}
+	rows, err := data.DB.Select(
+		db.Raw("lower(brand)"),
+		db.Raw("count(1)")).
+		From("products").
+		Where(
+			db.And(
+				db.Raw(`tsv @@ to_tsquery(?)`, strings.Join(parsed, "|")),
+				cond,
+			),
+		).
+		GroupBy(db.Raw("lower(brand)")).
+		Query()
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+
+	cache := make(map[string]int)
+	for rows.Next() {
+		var brand string
+		var count int
+
+		if err := rows.Scan(&brand, &count); err != nil {
+			break
+		}
+
+		cache[brand] = count
+	}
+
+	return cache
+}
+
 func NewDesignerList(ctx context.Context, products []*data.Product) []render.Renderer {
 	list := []render.Renderer{}
+
+	brands := []string{}
+	for _, p := range products {
+		brands = append(brands, p.Brand)
+	}
+	// assemble the tsquery from designers
+	var parsed []string
+	for _, d := range brands {
+		tt := splitRegex.Split(d, -1)
+		parsed = append(parsed, strings.Join(tt, "&"))
+	}
+
+	productCache := make(map[string][]render.Renderer)
+	for _, p := range fetchDesignerProducts(ctx, parsed) {
+		pp := p.(*Product)
+		b := strings.ToLower(pp.Brand)
+		if _, ok := productCache[b]; ok {
+			productCache[b] = []render.Renderer{}
+		}
+		productCache[b] = append(productCache[b], p)
+	}
+	lg.Warnf("%+v", productCache)
+	ctx = context.WithValue(ctx, "product.cache", productCache)
+
+	if cache := fetchDesignerCount(ctx, parsed); cache != nil {
+		ctx = context.WithValue(ctx, "count.cache", cache)
+	}
+
 	for _, p := range products {
 		list = append(list, NewDesigner(ctx, p))
 	}
