9bc7a02c062cc8d9598f4cb679e95817bd9d8e09
Paul Xue <xuepaul.xp@gmail.com>
2018-05-11 14:33:15 -0400

    hotfix: product status col

--- db/migrations/20180510114646_AddProductStatusColumn.sql
+++ db/migrations/20180510114646_AddProductStatusColumn.sql
@@ -2,7 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE products ADD COLUMN status SMALLINT DEFAULT 0 NOT NULL; 
-CREATE INDEX product_status_index ON products (product_status);
+CREATE INDEX product_status_index ON products (status);
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE products DROP COLUMN IF EXISTS product_status;
+ALTER TABLE products DROP COLUMN IF EXISTS status;
