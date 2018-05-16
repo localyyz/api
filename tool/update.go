e4c6e1a4e8c937fe0d8187809f0ed9715834c638
Paul Xue <xuepaul.xp@gmail.com>
2018-05-15 22:26:38 -0400

    image syncer job

--- tool/update.go
+++ tool/update.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/url"
 	"sync"
+	"time"
 
 	"bitbucket.org/moodie-app/moodie-api/data"
 	"bitbucket.org/moodie-app/moodie-api/lib/shopify"
@@ -11,75 +12,55 @@ import (
 	db "upper.io/db.v3"
 )
 
-type imageParse []*shopify.ProductImage
+type imageList []*data.ProductImage
+
+type workPackage struct {
+	client   *shopify.Client
+	page     int
+	imagechn chan imageList
+}
 
 func UpdateProductCategory(ctx context.Context) {
-	/* creating category cache */
-	categoryCache := make(map[string]*data.Category)
-	if categories, _ := data.DB.Category.FindAll(nil); categories != nil {
-		for _, c := range categories {
-			categoryCache[c.Value] = c
-		}
-	}
-
-	/* creating blacklist cache */
-	blacklistCache := make(map[string]*data.Blacklist)
-	if blacklist, _ := data.DB.Blacklist.FindAll(nil); blacklist != nil {
-		for _, word := range blacklist {
-			blacklistCache[word.Word] = word
-		}
-	}
-
-	var clients []*shopify.Client
-	var creds []*data.ShopifyCred
-	data.DB.Select(db.Raw("c.*")).
-		From("shopify_creds c").
-		LeftJoin("places p").On("p.id = c.place_id").
-		Where(db.Cond{"p.status": data.PlaceStatusActive}).
-		All(&creds)
-	for _, cred := range creds {
-		client := shopify.NewClient(nil, cred.AccessToken)
-		client.BaseURL, _ = url.Parse(cred.ApiURL)
-		clients = append(clients, client)
-	}
-
-	//lg.Infof("category: %d", len(categoryCache))
-	//lg.Infof("blacklist: %d", len(blacklistCache))
-	//ctx = context.WithValue(ctx, "category.cache", categoryCache)
-	//ctx = context.WithValue(ctx, "category.blacklist", blacklistCache)
-	//ctx = context.WithValue(ctx, "sync.place", &data.Place{})
-	//ctx = context.WithValue(ctx, "shopify.cache", shopifyClientCache)
-
-	imagesChann := make(chan imageParse, 10)
+	workpackageChan := make(chan workPackage)
+	imagechn := make(chan imageList)
 
 	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		lg.Printf("starting worker %d", i+1)
+	for i := 0; i < 20; i++ {
 		wg.Add(1)
-		go worker(ctx, imagesChann, wg)
+		go worker(ctx, workpackageChan, wg)
+		wg.Add(1)
+		go saver(ctx, imagechn, wg)
 	}
 
 	go func() {
-		for i, cl := range clients {
-			var (
-				page   = 0
-				limits = 250
-			)
-			for {
-				products, _, _ := cl.ProductList.Get(ctx, &shopify.ProductListParam{Limit: limits, Page: page})
-				if len(products) == 0 {
-					break
-				}
+		var creds []*data.ShopifyCred
+		data.DB.Select(db.Raw("c.*")).
+			From("shopify_creds c").
+			LeftJoin("places p").On("p.id = c.place_id").
+			Where(db.Cond{"p.status": data.PlaceStatusActive}).
+			All(&creds)
 
-				for _, p := range products {
-					imagesChann <- imageParse(p.Images)
-				}
+		for _, cred := range creds {
+			client := shopify.NewClient(nil, cred.AccessToken)
+			client.BaseURL, _ = url.Parse(cred.ApiURL)
 
-				page++
+			count, _, _ := client.ProductList.Count(ctx)
+			lg.Infof("place (%d) found %d products", cred.PlaceID, count)
+			var page int
+
+			for i := 0; i < count; i += 20 {
+				workpackageChan <- workPackage{
+					client:   client,
+					page:     page,
+					imagechn: imagechn,
+				}
+				page += 1
 			}
-			lg.Printf("sent page client %d", i)
+			lg.Infof("done place (%d)", cred.PlaceID)
 		}
-		close(imagesChann)
+
+		close(workpackageChan)
+		close(imagechn)
 	}()
 
 	// Wait for all HTTP fetches to complete.
@@ -87,51 +68,59 @@ func UpdateProductCategory(ctx context.Context) {
 	lg.Info("done.")
 }
 
-func worker(ctx context.Context, ch chan imageParse, wg sync.WaitGroup) {
-	//var updated []*data.Product
-	for images := range ch {
-		for _, image := range images {
-
-			//if product.Category.Type == 0 {
-			//parsedData := s.ParseProduct(ctx, product.Title, product.Description) //getting the category
-			//if len(parsedData.Value) > 0 {
-			//product.Category = data.ProductCategory{ //setting the category
-			//Type:  parsedData.Type,
-			//Value: parsedData.Value,
-			//}
-			//}
-			//}
-
-			//oldStatus := product.Status
-			//product.Status = finalizeStatus(ctx, len(product.Category.Value) > 0, product.Title)
-			//if product.Status == data.ProductStatusPending {
-			//product.Status = data.ProductStatusRejected
-			//}
-
-			//if oldStatus == product.Status {
-			//continue
-			//}
-
-			// find the product images
-			dbImage, err := data.DB.ProductImage.FindByExternalID(image.ID)
-			if err != nil {
-				continue
-			}
-			if dbImage.Width != image.Width || dbImage.Height != image.Height {
-				dbImage.Width = image.Width
-				dbImage.Height = image.Height
-				data.DB.ProductImage.Save(dbImage)
-			}
-
-			//saving it to the db
-			//if err := data.DB.Product.Save(product); err != nil { //updating db
-			//lg.Print("Error: Could not update the database entries")
-			//}
-			//updated = append(updated, product)
+func saver(ctx context.Context, list chan imageList, wg sync.WaitGroup) {
+	for l := range list {
+		lg.Infof("updating %d images", len(l))
+		for _, img := range l {
+			data.DB.ProductImage.Save(img)
 		}
-		//for _, p := range updated {
-		//lg.Printf("updated product (%d) to %s", p.ID, p.Status)
-		//}
+	}
+	wg.Done()
+}
+
+func worker(ctx context.Context, packages chan workPackage, wg sync.WaitGroup) {
+	for w := range packages {
+		s := time.Now()
+		products, _, _ := w.client.ProductList.Get(
+			ctx,
+			&shopify.ProductListParam{
+				Limit: 20,
+				Page:  w.page,
+			},
+		)
+		//lg.Infof("fetched in %v", time.Since(s))
+		if len(products) == 0 {
+			continue
+		}
+
+		externalIDs := []int64{}
+		imageMap := map[int64]*shopify.ProductImage{}
+		for _, p := range products {
+			for _, image := range p.Images {
+				externalIDs = append(externalIDs, image.ID)
+				imageMap[image.ID] = image
+			}
+		}
+
+		dbImages, err := data.DB.ProductImage.FindAll(db.Cond{"external_id": externalIDs})
+		if err != nil {
+			continue
+		}
+
+		var updates imageList
+		for _, img := range dbImages {
+			ext := imageMap[img.ExternalID]
+			if img.Width != ext.Width || img.Height != ext.Height {
+				img.Width = ext.Width
+				img.Height = ext.Height
+				updates = append(updates, img)
+			}
+		}
+
+		if len(updates) > 0 {
+			lg.Infof("sending %d updates in %v", len(updates), time.Since(s))
+			w.imagechn <- updates
+		}
 	}
 	wg.Done()
 }
