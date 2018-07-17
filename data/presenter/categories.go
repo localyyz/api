942340598b085f4595cfe8d4b10b22a70fb249d8
Paul Xue <xuepaul.xp@gmail.com>
2018-07-17 14:37:30 -0400

    search optimization

--- data/presenter/categories.go
+++ data/presenter/categories.go
@@ -16,26 +16,33 @@ type Category struct {
 	ctx context.Context
 }
 
+type subCatCache map[data.CategoryType][]string
+
 func NewCategory(ctx context.Context, categoryType data.CategoryType) *Category {
 	category := &Category{
 		Type: categoryType,
 		ctx:  ctx,
 	}
-	cond := db.Cond{
-		"type":       categoryType,
-		"mapping !=": "",
-	}
-	var categories []*data.Category
-	data.DB.
-		Select(db.Raw("distinct mapping")).
-		From("product_categories").
-		Where(cond).
-		OrderBy("mapping").
-		All(&categories)
+	if subcats, ok := ctx.Value("subcat").(subCatCache); ok {
+		category.Values = subcats[categoryType]
+	} else {
+		cond := db.Cond{
+			"type":       categoryType,
+			"mapping !=": "",
+		}
+		var categories []*data.Category
+		data.DB.
+			Select(db.Raw("distinct mapping")).
+			From("product_categories").
+			Where(cond).
+			OrderBy("mapping").
+			All(&categories)
 
-	category.Values = []string{}
-	for _, c := range categories {
-		category.Values = append(category.Values, c.Mapping)
+		category.Values = []string{}
+		for _, c := range categories {
+			category.Values = append(category.Values, c.Mapping)
+		}
+
 	}
 	return category
 }
@@ -48,6 +55,37 @@ type CategoryList []*Category
 
 func NewCategoryList(ctx context.Context, categories []data.CategoryType) []render.Renderer {
 	list := []render.Renderer{}
+
+	cond := db.Cond{
+		"mapping": db.NotEq(""),
+		"type":    categories,
+	}
+	if sessionUser, ok := ctx.Value("session.user").(*data.User); ok {
+		if sessionUser.Etc.Gender == data.UserGenderMale {
+			cond["gender"] = data.ProductGenderMale
+		} else if sessionUser.Etc.Gender == data.UserGenderFemale {
+			cond["gender"] = data.ProductGenderFemale
+		}
+	}
+	// bulk fetch product categories
+	rows, _ := data.DB.
+		Select(db.Raw("distinct mapping"), "type").
+		From("product_categories").
+		Where(cond).
+		OrderBy("mapping").
+		Query()
+
+	subcatMap := subCatCache{}
+	for rows.Next() {
+		var mapping string
+		var typ data.CategoryType
+		if err := rows.Scan(&mapping, &typ); err != nil {
+			break
+		}
+		subcatMap[typ] = append(subcatMap[typ], mapping)
+	}
+	ctx = context.WithValue(ctx, "subcat", subcatMap)
+
 	for _, c := range categories {
 		list = append(list, NewCategory(ctx, c))
 	}
