8b14c621ecdefc3cdb28c606cd35e5e12bc1217d
waseef <waseef@localyyz.com>
2018-08-03 16:48:50 -0400

    isFavourite flag
    add foreign key constraint into favourite_products table
    new isFavourite flag on each product

--- db/migrations/20180802123905_AddFavouriteProducts.sql
+++ db/migrations/20180802123905_AddFavouriteProducts.sql
@@ -8,6 +8,12 @@ CREATE TABLE favourite_products(
   UNIQUE(user_id, product_id)
 );
 
+ALTER TABLE ONLY public.favourite_products
+    ADD CONSTRAINT favourite_products_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.favourite_products
+    ADD CONSTRAINT favourite_products_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 DROP TABLE IF EXISTS favourite_products;
