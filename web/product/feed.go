b817c7bd391d900e31cdd14892f36608a5e9c772
Paul Xue <xuepaul.xp@gmail.com>
2018-08-14 09:49:26 -0400

    wrap product/feed with helper routes

--- web/product/feed.go
+++ web/product/feed.go
@@ -15,22 +15,28 @@ import (
 func ListRandomProduct(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	cursor := ctx.Value("cursor").(*api.Page)
+	filterSort := ctx.Value("filter.sort").(*api.FilterSort)
 
-	result := data.DB.Product.
-		Find(
-			db.Cond{
-				"status": data.ProductStatusApproved,
-			}).
+	cursor.Limit = 20 // hard coded
+	query := data.DB.Select("p.*").
+		From("products p").
+		Where(db.Cond{
+			"status": data.ProductStatusApproved,
+			"score":  db.Gte(4),
+		}).
 		OrderBy(db.Raw("RANDOM()"))
-	result = cursor.UpdateQueryUpper(result)
+	query = filterSort.UpdateQueryBuilder(query)
 
 	var products []*data.Product
-	if err := result.All(&products); err != nil {
-		render.Respond(w, r, err)
-		return
+	if !filterSort.HasFilter() {
+		paginate := cursor.UpdateQueryBuilder(query)
+		if err := paginate.All(&products); err != nil {
+			render.Respond(w, r, err)
+			return
+		}
+		cursor.Update(products)
 	}
 
-	cursor.Update(products)
 	presented := presenter.NewProductList(ctx, products)
 	if err := render.RenderList(w, r, presented); err != nil {
 		render.Respond(w, r, err)
