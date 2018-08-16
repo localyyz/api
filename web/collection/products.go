a40d58f14db47ded710c603b4d634744c389d755
Paul Xue <xuepaul.xp@gmail.com>
2018-08-16 17:30:59 -0400

    hotfix: organize first page of collection via hour

--- web/collection/products.go
+++ web/collection/products.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"bitbucket.org/moodie-app/moodie-api/data"
 	"bitbucket.org/moodie-app/moodie-api/data/presenter"
@@ -41,10 +42,11 @@ func ListProducts(w http.ResponseWriter, r *http.Request) {
 		db.Cond{"p.status": data.ProductStatusApproved},
 	)
 
+	t := time.Now().Truncate(time.Hour).Unix()
 	query := data.DB.Select(db.Raw("distinct p.*")).
 		From("products p").
 		Where(cond).
-		OrderBy("p.score DESC", "p.created_at DESC")
+		OrderBy(db.Raw(fmt.Sprintf("%d %% id", t)), "-score")
 
 	query = filterSort.UpdateQueryBuilder(query)
 	paginate := cursor.UpdateQueryBuilder(query)
