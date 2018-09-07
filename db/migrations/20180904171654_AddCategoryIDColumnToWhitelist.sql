3f94d2ecae504733b5b5921cb0345f7ca8e05e49
Paul Xue <xuepaul.xp@gmail.com>
2018-09-07 15:02:12 -0400

    fix: drop mapping from whitelist

--- db/migrations/20180904171654_AddCategoryIDColumnToWhitelist.sql
+++ db/migrations/20180904171654_AddCategoryIDColumnToWhitelist.sql
@@ -4,6 +4,7 @@ ALTER TABLE whitelist ADD COLUMN category_id bigint;
 ALTER TABLE whitelist ADD FOREIGN KEY (category_id) REFERENCES categories (id);
 ALTER TABLE whitelist DROP COLUMN image_url;
 ALTER TABLE whitelist DROP CONSTRAINT unique_product_category_value;
+ALTER TABLE whitelist DROP COLUMN mapping;
 ALTER TABLE whitelist ADD CONSTRAINT unique_whitelist_value_gender unique (value, gender);
 
 -- +goose Down
